7a284f05ea022e07be74f446ba0338abca21d2aa
Allison Summers <allisonsummers44@gmail.com>
2018-02-28 08:59:40 -0500

    adjusted faves cards

--- src/workouts/ArtworkFaves.js
+++ src/workouts/ArtworkFaves.js
@@ -2,35 +2,24 @@ import React from 'react';
 import { Button, Row, Col } from 'reactstrap';
 import NotesModal from './NotesModal';
 import './artwork.css'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const Image = styled.img`
-    object-fit: fit;
+// const Image = styled.img`
+//     max-height: 30%;
+//     max-width: auto;
 
-`
+// `
 
 
 const ArtworkFaves = (props) => {
-    // var openModal = (id) => {
-    //     console.log("OPPPENNNN", id)
-    //     if(id){
-    //          return (
-    //         <div><NotesModal artwork={id} isOpen={true}/>
-    //         {console.log("HEY this is returned")}
-    //         </div>
-    //     ) 
-    //     } else {
-    //         return null
-    //     }
-      
-    // }
+   
     var ArtworksRemapped = props.artworks.map((artwork, id) => {
         return (
 
             <div key={id} className="fav-content">
             <Row className="justify-content-center">
-                <Col>
-                <Image src={artwork.url} alt="fav" className="art-image"/>
+                <Col xs="9" className="container">
+                <img src={artwork.url} alt="fav" className="art-image"/>
                 </Col>
             </Row>
                 <hr/>
